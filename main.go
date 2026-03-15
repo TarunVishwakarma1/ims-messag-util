6625ee5860d88e651025512c98115c81ef33d8ce
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-03-15 15:43:47 +0530

    ADDED chi to make http service to trigger messages

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/notification/{notificationType}", notification)
+	http.ListenAndServe(":8080", r)
+}
+
+func notification(w http.ResponseWriter, r *http.Request) {
+
 }
